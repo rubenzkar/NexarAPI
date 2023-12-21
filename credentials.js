87b7e2478afe2bc317c163bd0c56a2f3cbc9bd42
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-20 19:30:16 -0600

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,7 +1,5 @@
 // credentials.js
 
-const credentials = {
+var credentials = {
   accessToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjA5NzI5QTkyRDU0RDlERjIyRDQzMENBMjNDNkI4QjJFIiwidHlwIjoiYXQrand0In0', // Replace with your actual static access token
 };
-
-export default credentials;
