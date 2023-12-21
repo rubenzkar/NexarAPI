4e8e76b96b1010056ac1adf33c1c164748b24e05
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-20 22:38:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// script.js
-
-/ Function to send GraphQL query
+// Function to send GraphQL query
 function sendQuery() {
-    // Get the user-input value for 'q'
     var userInput = document.getElementById('userInput').value;
-
-    // Hardcoded GraphQL query with a variable for 'q'
     var query = `
       query specAttributes($inputQ: String!) {
         supSearchMpn(q: $inputQ, limit: 1) {
@@ -31,14 +26,10 @@ function sendQuery() {
       }
     `;
 
-    // Destructure credentials
     var accessToken = credentials.accessToken;
-
-    // Make GraphQL Request using the static access token and user-input value for 'q'
     makeGraphQLRequest(query, userInput, accessToken);
 }
 
-// Function to make GraphQL request using the provided access token and user-input value for 'q'
 function makeGraphQLRequest(query, userInput, accessToken) {
     var graphqlEndpoint = 'https://api.nexar.com/graphql/';
 
@@ -48,73 +39,54 @@ function makeGraphQLRequest(query, userInput, accessToken) {
         },
     })
     .then(function(apiResponse) {
-        // Handle the GraphQL API response
         displayResponse(apiResponse.data);
     })
     .catch(function(error) {
-        // Handle errors in GraphQL request
         console.error('GraphQL Request Error:', error);
         displayError('Error making GraphQL request. Check console for details.');
     });
 }
 
-// Function to display error message to the user
 function displayError(message) {
     var errorContainer = document.getElementById('errorContainer');
     errorContainer.textContent = message;
     errorContainer.style.display = 'block';
 }
 
-// Function to display the JSON response attributes and values in an HTML table
 function displayResponse(response) {
     var responseTableContainer = document.getElementById('responseTableContainer');
     var responseTable = document.getElementById('responseTable');
-
-    // Clear previous table content
     responseTable.innerHTML = '';
 
-    // Display the table container
     responseTableContainer.style.display = 'block';
 
-    // Log the entire response object to the console for troubleshooting
-    console.log('GraphQL Response:', response);
-
-    // Check if 'supSearchMpn.results' property exists in the response
     if (response.data && response.data.supSearchMpn && response.data.supSearchMpn.results) {
-        // Extract the part details from the JSON response
         var partDetails = response.data.supSearchMpn.results[0]?.part;
 
-        // Check if 'part' property exists
         if (partDetails) {
-            // Define custom headers and clean-up functions
             var headers = {
                 mpn: 'MPN',
                 manufacturer: 'Manufacturer',
                 shortDescription: 'Description',
                 bestImage: 'Image',
-                specs: 'Specifications' // Updated header for 'specs'
+                specs: 'Specifications'
             };
 
             var cleanUpFunctions = {
                 mpn: function (value) {
-                    // Add any specific clean-up logic for mpn here if needed
                     return value;
                 },
                 manufacturer: function (value) {
-                    // Check if 'manufacturer' is an object and extract the 'name' property
                     return value && value.name ? value.name : value;
                 },
                 shortDescription: function (value) {
-                    // Add any specific clean-up logic for shortDescription here if needed
                     return value;
                 },
                 bestImage: function (value) {
-                    // Check if 'bestImage' is an object and extract the 'url' property
                     return value && value.url ? `<img src="${value.url}" alt="Product Image" style="max-width: 100px; max-height: 100px;">` : value;
                 }
             };
 
-            // Create rows for 'MPN,' 'Manufacturer,' 'Description,' and 'Image'
             Object.keys(partDetails).forEach(function (attribute) {
                 if (attribute !== 'specs') {
                     var tr = responseTable.insertRow();
@@ -123,7 +95,6 @@ function displayResponse(response) {
 
                     attributeCell.textContent = headers[attribute] || attribute;
 
-                    // Clean up the value if a clean-up function is provided
                     var cleanedValue = cleanUpFunctions[attribute] ? cleanUpFunctions[attribute](partDetails[attribute]) : partDetails[attribute];
                     valueCell.innerHTML = cleanedValue;
                 }
@@ -135,7 +106,6 @@ function displayResponse(response) {
             specsHeaderCell.colSpan = 2;
             specsHeaderCell.textContent = headers.specs;
 
-            // Create rows for each spec attribute and value
             partDetails.specs.forEach(function (spec) {
                 var specRow = responseTable.insertRow();
                 var specAttributeCell = specRow.insertCell(0);
