85c747079781d16b8087ae1e46e7b24f8fb59a01
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-21 09:25:26 -0600

    Update compare.js

--- compare.js
+++ compare.js
@@ -17,83 +17,6 @@ function sendGraphQLQuery(query, url, type, accessToken) {
     });
 }
 
-// Function to display GraphQL response for comparison
-function displayComparison(response, type, url) {
-    var responseTableContainer = document.getElementById('responseTableContainer');
-
-    // Create a table for each response
-    var table = document.createElement('table');
-    table.innerHTML = '<caption>' + type + ' URL: ' + url + '</caption>';
-
-    if (response.data && response.data.supSearchMpn && response.data.supSearchMpn.results) {
-        var partDetails = response.data.supSearchMpn.results[0]?.part;
-
-        if (partDetails) {
-            var headers = {
-                mpn: 'MPN',
-                manufacturer: 'Manufacturer',
-                shortDescription: 'Description',
-                bestImage: 'Image',
-                specs: 'Specifications'
-            };
-
-            var cleanUpFunctions = {
-                mpn: function (value) {
-                    return value;
-                },
-                manufacturer: function (value) {
-                    return value && value.name ? value.name : value;
-                },
-                shortDescription: function (value) {
-                    return value;
-                },
-                bestImage: function (value) {
-                    return value && value.url ? `<img src="${value.url}" alt="Product Image" style="max-width: 100px; max-height: 100px;">` : value;
-                }
-            };
-
-            Object.keys(partDetails).forEach(function (attribute) {
-                if (attribute !== 'specs' && attribute !== 'bestDatasheet') {
-                    var tr = table.insertRow();
-                    var attributeCell = tr.insertCell(0);
-                    var valueCell = tr.insertCell(1);
-
-                    attributeCell.textContent = headers[attribute] || attribute;
-
-                    var cleanedValue = cleanUpFunctions[attribute] ? cleanUpFunctions[attribute](partDetails[attribute]) : partDetails[attribute];
-                    valueCell.innerHTML = cleanedValue;
-                }
-            });
-
-            var specsHeaderRow = table.insertRow();
-            var specsHeaderCell = specsHeaderRow.insertCell(0);
-            specsHeaderCell.colSpan = 2;
-
-            var specsHeaderElement = document.createElement('h3');
-            specsHeaderElement.textContent = headers.specs;
-
-            specsHeaderCell.appendChild(specsHeaderElement);
-
-            partDetails.specs.forEach(function (spec) {
-                var specRow = table.insertRow();
-                var specAttributeCell = specRow.insertCell(0);
-                var specValueCell = specRow.insertCell(1);
-
-                specAttributeCell.textContent = spec.attribute.name;
-                specValueCell.textContent = spec.displayValue;
-            });
-        } else {
-            console.error('Invalid response format: "part" property is missing or empty.');
-            displayError('Invalid response format for ' + type + '. Check console for details.');
-        }
-    } else {
-        console.error('Invalid response format: "supSearchMpn.results" property is missing.');
-        displayError('Invalid response format for ' + type + '. Check console for details.');
-    }
-
-    responseTableContainer.appendChild(table);
-}
-
 // Function to compare GraphQL responses
 function compareResponses() {
     var referenceInput = document.getElementById('reference').value.trim();
@@ -138,4 +61,66 @@ function compareResponses() {
     sendGraphQLQuery(query, referenceInput, 'reference', accessToken);
     sendGraphQLQuery(query, alternateInput, 'alternate', accessToken);
 
+}
+
+// Function to display GraphQL response for comparison
+function displayComparison(response, type, url) {
+    var responseTableContainer = document.getElementById('responseTableContainer');
+
+    // Create a table for each response
+    var table = document.createElement('table');
+    table.innerHTML = '<caption>' + type + ' URL: ' + url + '</caption>';
+
+    if (response.data && response.data.supSearchMpn && response.data.supSearchMpn.results) {
+        var partDetails = response.data.supSearchMpn.results[0]?.part;
+
+        if (partDetails) {
+            var headers = {
+                mpn: 'MPN',
+                manufacturer: 'Manufacturer',
+                shortDescription: 'Description',
+                bestImage: 'Image',
+                specs: 'Specifications'
+            };
+
+            var cleanUpFunctions = {
+                mpn: function (value) {
+                    return value;
+                },
+                manufacturer: function (value) {
+                    return value && value.name ? value.name : value;
+                },
+                shortDescription: function (value) {
+                    return value;
+                },
+                bestImage: function (value) {
+                    return value && value.url ? `<img src="${value.url}" alt="Product Image" style="max-width: 100px; max-height: 100px;">` : value;
+                }
+            };
+
+            // Create header row
+            var headerRow = table.insertRow();
+            Object.values(headers).forEach(function (header) {
+                var headerCell = headerRow.insertCell();
+                headerCell.textContent = header;
+            });
+
+            // Create data rows
+            var dataRow = table.insertRow();
+            Object.keys(headers).forEach(function (key) {
+                var dataCell = dataRow.insertCell();
+                var cleanedValue = cleanUpFunctions[key] ? cleanUpFunctions[key](partDetails[key]) : partDetails[key];
+                dataCell.innerHTML = cleanedValue;
+            });
+
+            // Display the table
+            responseTableContainer.appendChild(table);
+        } else {
+            console.error('Invalid response format: "part" property is missing or empty.');
+            displayError('Invalid response format for ' + type + '. Check console for details.');
+        }
+    } else {
+        console.error('Invalid response format: "supSearchMpn.results" property is missing.');
+        displayError('Invalid response format for ' + type + '. Check console for details.');
+    }
 }
