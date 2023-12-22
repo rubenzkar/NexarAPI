b60e7421284ea1d94949b17dd84362b3a015b1d4
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-22 01:59:23 -0600

    Update sbs.js

--- sbs.js
+++ sbs.js
@@ -99,7 +99,7 @@ async function fetchAttribute(type, specValue) {
             throw new Error(`Attribute '${specValue}' not found.`);
         }
 
-        console.log('${specValue} value of '${part.mpn}':', attribute.displayValue);
+        console.log(${specValue}' value of '${part.mpn}':', attribute.displayValue);
     } catch (error) {
         console.error(error.message);
     }
