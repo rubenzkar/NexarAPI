6ac7e3892b1110b4a787fa66d2cff447aed481e8
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-22 10:36:08 -0600

    Update sbs.js

--- sbs.js
+++ sbs.js
@@ -2,7 +2,7 @@ const GRAPHQL_ENDPOINT = 'https://api.nexar.com/graphql/';
 const urlParams = new URLSearchParams(window.location.search);
 const reference = urlParams.get('reference');
 const alternate = urlParams.get('alternate');
-const accessToken = credentials.accessToken;
+const accessToken = credentials.accessToken; 
 
 // Function to perform GraphQL query and return response
 async function getGraphQLResponse(query, variables) {
@@ -12,8 +12,7 @@ async function getGraphQLResponse(query, variables) {
                 Authorization: `Bearer ${accessToken}`,
             },
         });
-
-        console.log('GraphQL Response:', response.data);
+        //console.log('GraphQL Response:', response.data);
         return response.data;
     } catch (error) {
         console.error('GraphQL Request Error:', error);
@@ -57,14 +56,11 @@ async function getPart(type) {
         if (!response) {
             throw new Error('Error getting GraphQL response.');
         }
-        
         const part = response?.data?.supSearchMpn?.results[0]?.part;
-
         if (!part) {
             throw new Error('Error retrieving part values from GraphQL response.');
         }
-
-        console.log('Part Values:', part);
+        console.log('Part:', part);
         return part;
     } catch (error) {
         console.error(error.message);
@@ -73,50 +69,53 @@ async function getPart(type) {
 }
 
 // Function to get part specs
-function getPartSpecs(part) {
+function getSpecs(part) {
     const specs = part?.specs;
 
     if (!specs) {
         throw new Error('Error retrieving specs from part values.');
     }
-
     console.log('Specs:', specs);
     return specs;
 }
 
-async function fetchAttribute(type, specValue) {
-    let part, specs;
-
+function getAttribute(specs, specValue) {
     try {
-        if (type === 'reference') {
-            part = await getPart(reference);
-            specs = getPartSpecs(part);
-        } else if (type === 'alternate') {
-            part = await getPart(alternate);
-            specs = getPartSpecs(part);
-        }
-
         const attribute = specs.find(spec => spec.attribute.name === specValue);
 
         if (!attribute) {
-            throw new Error('Attribute ' + specValue+ ' not found.');
+            throw new Error(`Attribute ${specValue} not found.`);
         }
 
-        console.log(specValue + ' value of ' + part.mpn + ': ' + attribute.displayValue);
-
+        //console.log(`${specValue} value: ${attribute.displayValue}`);
+        return attribute.displayValue;
     } catch (error) {
         console.error(error.message);
     }
 }
 
-async function main() {
+async function compareResponses() {
     try {
-        fetchAttribute('reference', 'Capacitance');
-        fetchAttribute('alternate', 'Capacitance');
+        const refPart = await getPart(reference);
+        const altPart = await getPart(alternate);
+
+        const refSpecs = getSpecs(refPart);
+        const altSpecs = getSpecs(altPart);
+
         
+        var refMpn = refPart.mpn;
+        var refManufacturer = refPart.manufacturer.name;
+        var refCapValue = getAttribute(refSpecs, 'Capacitance');
+
+        var altMpn = altPart.mpn;
+        var altManufacturer = altPart.manufacturer.name;        
+        var altCapValue = getAttribute(altSpecs, 'Capacitance');
+
+        console.log(refManufacturer+ "'s " + refMpn + ' cap value: ' + refCapValue);
+        console.log(altManufacturer+ "'s " + altMpn + ' cap value: ' + altCapValue);
     } catch (error) {
         console.error(error.message);
     }
 }
 
-main();
+compareResponses();
