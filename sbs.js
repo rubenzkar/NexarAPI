2b5a7181ed82af826782c7b5f33319b052699814
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-22 20:50:30 -0600

    Update sbs.js

--- sbs.js
+++ sbs.js
@@ -22,7 +22,7 @@ async function getGraphQLResponse(query, variables) {
 
 // Function to get part values
 async function getPart(type) {
-    const query = `
+    var query = `
         query specAttributes($inputQ: String!) {
             supSearchMpn(q: $inputQ, limit: 1) {
                 results {
@@ -44,7 +44,7 @@ async function getPart(type) {
                         bestDatasheet {
                             url
                         }
-                         medianPrice1000{
+                        medianPrice1000{
                           price
                         }
                     }
