5a3c9a19f2639b5e12a29ec228a4774d3552e9e7
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-23 19:30:37 -0600

    Update compare.js

--- compare.js
+++ compare.js
@@ -1,6 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
 const reference = urlParams.get('reference');
 const alternate = urlParams.get('alternate');
+const credentialsArray = [
+    // Szocialis Networks
+    { client: '901f869b-6cb4-4910-9a4f-6ee1738baee6', secret: '09KGnzVSXu_wdyCXSEqZksLv01ItpNtcd2SJ' },
+    // Ruben Escarzaga
+    { client: '6fbf42ca-731d-4434-bd02-9fdd3c0814e6', secret: 'zPsDKpUVW3ihd-n6T_G6mc9VeybDScY9Pi5U' },
+    //Zephyr Technologies
+    //{ client: '2fbc50ad-4999-47b2-b523-616a509a66d0', secret: 'dek_wdMgzVT9zlKHvwJw1SjvmjYYC18fOiVM' },
+    //zkar Communications
+    { client: '054a1a2c-b945-4724-bb00-5fec7ba942b5', secret: '6TdqiOgjdqfZNrZpkmU5Cuw-1_zHPZPcoYEg' }
+];
 const credentials = getRandomCredentials();
 const CLIENT_ID = credentials.client;
 const CLIENT_SECRET = credentials.secret;
