2181fd9e4cb5c36df4989574477acd36657ba99e
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-23 19:32:25 -0600

    Update comparing.js

--- comparing.js
+++ comparing.js
@@ -1,10 +1,39 @@
 const urlParams = new URLSearchParams(window.location.search);
 const reference = urlParams.get('reference');
 const alternate = urlParams.get('alternate');
-const CLIENT_ID = "901f869b-6cb4-4910-9a4f-6ee1738baee6";
-const CLIENT_SECRET = "09KGnzVSXu_wdyCXSEqZksLv01ItpNtcd2SJ";
+const credentialsArray = [
+    // Szocialis Networks
+    { client: '901f869b-6cb4-4910-9a4f-6ee1738baee6', secret: '09KGnzVSXu_wdyCXSEqZksLv01ItpNtcd2SJ' },
+    // Ruben Escarzaga
+    { client: '6fbf42ca-731d-4434-bd02-9fdd3c0814e6', secret: 'zPsDKpUVW3ihd-n6T_G6mc9VeybDScY9Pi5U' },
+    //Zephyr Technologies
+    //{ client: '2fbc50ad-4999-47b2-b523-616a509a66d0', secret: 'dek_wdMgzVT9zlKHvwJw1SjvmjYYC18fOiVM' },
+    //zkar Communications
+    { client: '054a1a2c-b945-4724-bb00-5fec7ba942b5', secret: '6TdqiOgjdqfZNrZpkmU5Cuw-1_zHPZPcoYEg' }
+];
+const credentials = getRandomCredentials();
+const CLIENT_ID = credentials.client;
+const CLIENT_SECRET = credentials.secret;
 const TOKEN_ENDPOINT = "https://identity.nexar.com/connect/token"; 
 const GRAPHQL_ENDPOINT = "https://api.nexar.com/graphql"; 
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function getRandomCredentials() {
+    // Make a copy to avoid modifying the original constant array
+    const shuffledArray = [...credentialsArray];
+    
+    // Shuffle the array
+    shuffleArray(shuffledArray);
+
+    // Return the first pair (now random)
+    return shuffledArray[0];
+}
 
 async function getAccessToken() {
   try {
