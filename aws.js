33d45b9eb8519d87116d3584876168bb95d32593
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-24 11:13:25 -0600

    Delete aws.js

--- aws.js
+++
@@ -1,4 +0,0 @@
-fetch('https://kc0sqceqeb.execute-api.us-west-2.amazonaws.com')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
