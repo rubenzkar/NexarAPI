0aa9647bcf0f9455a0b76753ed7a59d6aadd6798
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-25 13:38:37 -0600

    Update comparing.js

--- comparing.js
+++ comparing.js
@@ -190,98 +190,99 @@ function buyNow (alternate){
 
 // Function to display the comparison table
 async function displayComparisonTable() {
-    // Display loading bar
-    var loadingBar = new ldBar(".ldBar", {
-     "stroke": '#f00',
-     "stroke-width": 10,
-     "preset": "fan",
-     "value": 50
-    });
-    loadingBar.style.display = 'block';
-
+  // Display loading bar    
+  var loadingBar = document.getElementById('loadingBar');
+  var bar = new ldBar(".ldBar", {
+    "stroke": '#f00',
+    "stroke-width": 10,
+    "preset": "fan",
+    "value": 50
+  });
+  loadingBar.style.display = 'block';
   
-    const table = document.getElementById('responseTable');
-    //Get parts
-    const parts = await getParts(reference, alternate);
-    const refPart = await getPart(parts,'ref');
-    const altPart = await getPart(parts,'alt');
-    // Get specs
-    const refSpecs = getSpecs(refPart);
-    const altSpecs = getSpecs(altPart);
-    //Get values for Ref
-    var refManufacturer = refPart.manufacturer.name;
-    var refMpn = refPart.mpn;
-    var refImage = refPart.bestImage;
-    var refDesc = refPart.shortDescription;
-    var refCapValue = getAttribute(refSpecs, 'Capacitance');
-    var refTolValue = getAttribute(refSpecs, 'Tolerance');
-    var refVolValue = getAttribute(refSpecs, 'Voltage Rating');
-    var refLifeValue = getAttribute(refSpecs, 'Life (Hours)');
-    var refLeakValue = getAttribute(refSpecs, 'Leakage Current');
-    var refHeightValue = getAttribute(refSpecs, 'Height');
-    var refLengthValue = getAttribute(refSpecs, 'Length');
-    var refPrice = refPart.medianPrice1000.price;
-    //Get values for Alt
-    var altManufacturer = altPart.manufacturer.name;
-    var altMpn = altPart.mpn;
-    var altImage = altPart.bestImage;
-    var altDesc = altPart.shortDescription;
-    var altCapValue = getAttribute(altSpecs, 'Capacitance');
-    var altTolValue = getAttribute(altSpecs, 'Tolerance');
-    var altVolValue = getAttribute(altSpecs, 'Voltage Rating');
-    var altLifeValue = getAttribute(altSpecs, 'Life (Hours)');
-    var altLeakValue = getAttribute(altSpecs, 'Leakage Current');
-    var altHeightValue = getAttribute(altSpecs, 'Height');
-    var altLengthValue = getAttribute(altSpecs, 'Length');
-    var altPrice = altPart.medianPrice1000.price;
-    // Create rows for each part attribute
-    const manufacturerRow = createTableRow('Manufacturer', refManufacturer, altManufacturer);
-    const mpnRow = createTableRow('MPN', refMpn, altMpn);
-    const imageRow = createTableRow('Image', refImage ? `<img src="${refImage.url}" alt="Product Image" style="max-width: 100px; max-height: 100px;">` : '', altImage ? `<img src="${altImage.url}" alt="Product Image" style="max-width: 100px; max-height: 100px;">` : '');
-    const descRow = createTableRow('Description', refDesc, altDesc);
-    const capValueRow = createTableRow('Capacitance', refCapValue, altCapValue);
-    const tolValueRow = createTableRow('Tolerance', refTolValue, altTolValue);
-    const volValueRow = createTableRow('Voltage Rating', refVolValue, altVolValue);
-    const lifeValueRow = createTableRow('Life (Hours)', refLifeValue, altLifeValue);
-    const leakValueRow = createTableRow('Leakage Current', refLeakValue, altLeakValue);
-    const heightValueRow = createTableRow('Height', refHeightValue, altHeightValue);
-    const lengthValueRow = createTableRow('Length', refLengthValue, altLengthValue);
-    const priceRow = createTableRow('Price', '$' + refPrice, '$' + altPrice);
-    const buyRow = createTableRow('', '', '<button type="button" onclick="buyNow(' + "'"+ alternate + "'"+ ')">Buy Now</button>');
-
-    // Append rows to the table
-    table.appendChild(manufacturerRow);
-    table.appendChild(mpnRow);
-    table.appendChild(imageRow);
-    table.appendChild(descRow);
-    table.appendChild(capValueRow);
-    table.appendChild(tolValueRow);
-    table.appendChild(volValueRow);
-    table.appendChild(lifeValueRow);
-    table.appendChild(leakValueRow);
-    table.appendChild(heightValueRow);
-    table.appendChild(lengthValueRow);
-    table.appendChild(priceRow);
-    table.appendChild(buyRow);
-
+  
+  const table = document.getElementById('responseTable');
+  //Get parts
+  const parts = await getParts(reference, alternate);
+  const refPart = await getPart(parts,'ref');
+  const altPart = await getPart(parts,'alt');
+  // Get specs
+  const refSpecs = getSpecs(refPart);
+  const altSpecs = getSpecs(altPart);
+  //Get values for Ref
+  var refManufacturer = refPart.manufacturer.name;
+  var refMpn = refPart.mpn;
+  var refImage = refPart.bestImage;
+  var refDesc = refPart.shortDescription;
+  var refCapValue = getAttribute(refSpecs, 'Capacitance');
+  var refTolValue = getAttribute(refSpecs, 'Tolerance');
+  var refVolValue = getAttribute(refSpecs, 'Voltage Rating');
+  var refLifeValue = getAttribute(refSpecs, 'Life (Hours)');
+  var refLeakValue = getAttribute(refSpecs, 'Leakage Current');
+  var refHeightValue = getAttribute(refSpecs, 'Height');
+  var refLengthValue = getAttribute(refSpecs, 'Length');
+  var refPrice = refPart.medianPrice1000.price;
+  //Get values for Alt
+  var altManufacturer = altPart.manufacturer.name;
+  var altMpn = altPart.mpn;
+  var altImage = altPart.bestImage;
+  var altDesc = altPart.shortDescription;
+  var altCapValue = getAttribute(altSpecs, 'Capacitance');
+  var altTolValue = getAttribute(altSpecs, 'Tolerance');
+  var altVolValue = getAttribute(altSpecs, 'Voltage Rating');
+  var altLifeValue = getAttribute(altSpecs, 'Life (Hours)');
+  var altLeakValue = getAttribute(altSpecs, 'Leakage Current');
+  var altHeightValue = getAttribute(altSpecs, 'Height');
+  var altLengthValue = getAttribute(altSpecs, 'Length');
+  var altPrice = altPart.medianPrice1000.price;
+  // Create rows for each part attribute
+  const manufacturerRow = createTableRow('Manufacturer', refManufacturer, altManufacturer);
+  const mpnRow = createTableRow('MPN', refMpn, altMpn);
+  const imageRow = createTableRow('Image', refImage ? `<img src="${refImage.url}" alt="Product Image" style="max-width: 100px; max-height: 100px;">` : '', altImage ? `<img src="${altImage.url}" alt="Product Image" style="max-width: 100px; max-height: 100px;">` : '');
+  const descRow = createTableRow('Description', refDesc, altDesc);
+  const capValueRow = createTableRow('Capacitance', refCapValue, altCapValue);
+  const tolValueRow = createTableRow('Tolerance', refTolValue, altTolValue);
+  const volValueRow = createTableRow('Voltage Rating', refVolValue, altVolValue);
+  const lifeValueRow = createTableRow('Life (Hours)', refLifeValue, altLifeValue);
+  const leakValueRow = createTableRow('Leakage Current', refLeakValue, altLeakValue);
+  const heightValueRow = createTableRow('Height', refHeightValue, altHeightValue);
+  const lengthValueRow = createTableRow('Length', refLengthValue, altLengthValue);
+  const priceRow = createTableRow('Price', '$' + refPrice, '$' + altPrice);
+  const buyRow = createTableRow('', '', '<button type="button" onclick="buyNow(' + "'"+ alternate + "'"+ ')">Buy Now</button>');
+  
+  // Append rows to the table
+  table.appendChild(manufacturerRow);
+  table.appendChild(mpnRow);
+  table.appendChild(imageRow);
+  table.appendChild(descRow);
+  table.appendChild(capValueRow);
+  table.appendChild(tolValueRow);
+  table.appendChild(volValueRow);
+  table.appendChild(lifeValueRow);
+  table.appendChild(leakValueRow);
+  table.appendChild(heightValueRow);
+  table.appendChild(lengthValueRow);
+  table.appendChild(priceRow);
+  table.appendChild(buyRow);
+  
   // Hide loading bar once the table is loaded
-    loadingBar.ldBar.set(100);
-    loadingBar.style.display = 'none';
-}
-
-function displayError(message) {
+  loadingBar.ldBar.set(100);
+  loadingBar.style.display = 'none';
+  }
+  
+  function displayError(message) {
     var errorContainer = document.getElementById('errorContainer');
     errorContainer.textContent = message;
     errorContainer.style.display = 'block';
-}
-
-function compareResponses() {
-    try {
-        // Display the comparison table
-        displayComparisonTable();
-    } catch (error) {
-        console.error(error.message);
-    }
+  }
+  
+  function compareResponses() {
+  try {
+      // Display the comparison table
+      displayComparisonTable();
+  } catch (error) {
+      console.error(error.message);
+  }
 }
 
 compareResponses();
