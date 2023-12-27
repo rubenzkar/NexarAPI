a780f19d4c7bc3497273874948ac2d9cde1a881e
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-27 07:58:48 -0600

    Update jsonToHtmlTable.js

--- jsonToHtmlTable.js
+++ jsonToHtmlTable.js
@@ -18,14 +18,13 @@ function jsonToHtmlTable(jsonData) {
         } else if (key === 'mpn') {
             headerCell.innerHTML = 'MPN';
         } else if (key === 'bestDatasheet') {
-            // Handling the 'bestDatasheet' attribute which is an object
             headerCell.innerHTML = 'Datasheet';
         } else if (key === 'shortDescription') {
-            // Handling the 'shortDescription' attribute
             headerCell.innerHTML = 'Description';
         } else if (key === 'specs') {
-            // Handling the 'specs' attribute
             headerCell.innerHTML = 'Specs';
+        } else if (key === 'category') {
+            headerCell.innerHTML = 'Category';
         } else {
             headerCell.innerHTML = key;
         }
@@ -47,6 +46,8 @@ function jsonToHtmlTable(jsonData) {
                 cell.innerHTML = `<a href="${part[key].url}" target="_blank">${part[key].url}</a>`;
             } else if (key === 'specs') {
                 cell.innerHTML = formatSpecs(part[key]);
+            } else if (key === 'category') {
+                cell.innerHTML = part[key].name || part[key];
             } else {
                 cell.innerHTML = part[key];
             }
