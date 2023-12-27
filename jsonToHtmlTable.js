e33c4641bbf8554fa70aea1f8112e4c73e7e1880
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-26 21:30:24 -0600

    Update jsonToHtmlTable.js

--- jsonToHtmlTable.js
+++ jsonToHtmlTable.js
@@ -8,24 +8,32 @@ function jsonToHtmlTable(jsonData) {
 
     // Create header row
     var headerRow = table.insertRow(0);
+    var headerArr = [];
+
     for (var key in results) {
         var headerCell = headerRow.insertCell(-1);
 
         if (key === 'manufacturer') {
             headerCell.innerHTML = 'Manufacturer';
+            headerArr.push('Manufacturer');
         } else if (key === 'mpn') {
             headerCell.innerHTML = 'MPN';
+            headerArr.push('MPN');
         } else if (key === 'bestDatasheet') {
             // Handling the 'bestDatasheet' attribute which is an object
             headerCell.innerHTML = 'Datasheet';
+            headerArr.push('Datasheet');
         } else if (key === 'shortDescription') {
             // Handling the 'shortDescription' attribute
             headerCell.innerHTML = 'Description';
+            headerArr.push('Description');
         } else if (key === 'specs') {
             // Handling the 'specs' attribute
             headerCell.innerHTML = 'Specs';
+            headerArr.push('Specs');
         } else {
             headerCell.innerHTML = key;
+            headerArr.push(key);
         }
     }
 
@@ -54,6 +62,9 @@ function jsonToHtmlTable(jsonData) {
         }
     }
 
+    // Downloadable CSV file
+    downloadCSV(jsonData, headerArr);
+
     return table.outerHTML;
 }
 
@@ -64,4 +75,31 @@ function formatSpecs(specs) {
     }).join(',');
 
     return formattedSpecs;
+}
+
+function downloadCSV(jsonData, headers) {
+    var csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += headers.join(',') + '\n';
+
+    jsonData.forEach(function (row) {
+        var values = headers.map(function (header) {
+            if (header === 'bestDatasheet') {
+                return row.part[header].url;
+            } else if (header === 'specs') {
+                return formatSpecs(row.part[header]);
+            } else if (header === 'manufacturer') {
+                return row.part[header].name || row.part[header];
+            } else {
+                return row.part[header];
+            }
+        });
+        csvContent += values.join(',') + '\n';
+    });
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "data.csv");
+    document.body.appendChild(link);
+    link.click();
 }
