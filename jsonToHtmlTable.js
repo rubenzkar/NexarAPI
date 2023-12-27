ba6eaf0f51c1177524656b633e2d29634af949ab
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-26 19:42:00 -0600

    Update jsonToHtmlTable.js

--- jsonToHtmlTable.js
+++ jsonToHtmlTable.js
@@ -3,20 +3,34 @@
 function jsonToHtmlTable(jsonData) {
     var table = document.createElement('table');
 
-    // Assuming the first element in the array contains the necessary attributes
-    var headerRow = table.insertRow(0);
+    // Extracting the first element to gather header information
+    var firstElement = jsonData[0].part;
 
-    for (var key in jsonData[0].part) {
+    // Create header row
+    var headerRow = table.insertRow(0);
+    for (var key in firstElement) {
         var headerCell = headerRow.insertCell(-1);
         headerCell.innerHTML = key;
     }
 
+    // Populate data rows
     for (var i = 0; i < jsonData.length; i++) {
         var dataRow = table.insertRow(-1);
+        var part = jsonData[i].part;
 
-        for (var key in jsonData[i].part) {
+        for (var key in firstElement) {
             var cell = dataRow.insertCell(-1);
-            cell.innerHTML = jsonData[i].part[key].displayValue || jsonData[i].part[key];
+
+            if (key === 'manufacturer') {
+                cell.innerHTML = part[key].name || part[key];
+            } else if (key === 'specs') {
+                // Handling the 'specs' attribute which is an array of objects
+                var specsArray = part[key];
+                var specsString = specsArray.map(spec => spec.attribute.name + ": " + spec.displayValue).join(", ");
+                cell.innerHTML = specsString;
+            } else {
+                cell.innerHTML = part[key];
+            }
         }
     }
 
