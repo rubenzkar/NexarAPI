90b1774d510a4d1680864022872eef3f10a32d3c
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-27 07:55:00 -0600

    Update jsonToHtmlTable.js

--- jsonToHtmlTable.js
+++ jsonToHtmlTable.js
@@ -40,15 +40,12 @@ function jsonToHtmlTable(jsonData) {
             var cell = dataRow.insertCell(-1);
 
             if (key === 'shortDescription') {
-                // Handling the 'shortDescription' attribute
                 cell.innerHTML = part[key];
             } else if (key === 'manufacturer') {
                 cell.innerHTML = part[key].name || part[key];
             } else if (key === 'bestDatasheet') {
-                // Handling the 'bestDatasheet' attribute which is an object
                 cell.innerHTML = `<a href="${part[key].url}" target="_blank">${part[key].url}</a>`;
             } else if (key === 'specs') {
-                // Handling the 'specs' attribute
                 cell.innerHTML = formatSpecs(part[key]);
             } else {
                 cell.innerHTML = part[key];
@@ -73,46 +70,4 @@ function formatSpecs(specs) {
     }).join(',');
 
     return `{${formattedSpecs}}`;
-}
-
-// Function to download the CSV file
-function downloadCSVFile(csv_data) {
-  const csvFile = new Blob([csv_data], { type: "text/csv" });
-  const downloadLink = document.createElement("a");
-  downloadLink.download = "table_data.csv";
-  downloadLink.href = window.URL.createObjectURL(csvFile);
-
-  // Append the link to the document body if available; otherwise, append it to the document head
-  const parentElement = document.body || document.head;
-
-  if (parentElement) {
-    parentElement.appendChild(downloadLink);
-    downloadLink.click();
-    parentElement.removeChild(downloadLink); // Cleanup after click
-  } else {
-    console.error("Unable to find a valid parent element to append the download link.");
-  }
-}
-
-// Function to export the HTML table data to a CSV file
-function exportTableToCSV(tableId) {
-  const rows = document.querySelectorAll(tableId + " tr");
-  let csv_data = []; // Change from const to let
-
-  // Iterate through rows and columns, extract text data from each cell, and convert it to a comma-separated value
-  rows.forEach((row) => {
-    const cols = row.querySelectorAll("td, th");
-    const csvRow = [];
-    cols.forEach((col) => {
-      const textData = col.innerText;
-      csvRow.push(`"${textData}"`); // Wrap each value in double quotes
-    });
-    csv_data.push(csvRow.join(","));
-  });
-
-  // Combine each row data with a new line character
-  csv_data = csv_data.join("\n");
-
-  // Call the downloadCSVFile function to download the CSV file
-  downloadCSVFile(csv_data);
 }
