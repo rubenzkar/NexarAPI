ebf74456a6e9c740e456690898bb5036e81ecc71
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2023-12-26 20:14:31 -0600

    Update jsonToHtmlTable.js

--- jsonToHtmlTable.js
+++ jsonToHtmlTable.js
@@ -9,12 +9,12 @@ function jsonToHtmlTable(jsonData) {
     // Create header row
     var headerRow = table.insertRow(0);
     for (var key in firstElement) {
+        var headerCell = headerRow.insertCell(-1);
+
         if (key === 'bestDatasheet') {
             // Handling the 'bestDatasheet' attribute which is an object
-            var datasheetHeaderCell = headerRow.insertCell(-1);
-            datasheetHeaderCell.innerHTML = key;
+            headerCell.innerHTML = 'Datasheet Link';
         } else {
-            var headerCell = headerRow.insertCell(-1);
             headerCell.innerHTML = key;
         }
     }
@@ -31,9 +31,8 @@ function jsonToHtmlTable(jsonData) {
                 cell.innerHTML = part[key].name || part[key];
             } else if (key === 'bestDatasheet') {
                 // Handling the 'bestDatasheet' attribute which is an object
-                var datasheet = part[key];
                 var datasheetCell = dataRow.insertCell(-1);
-                datasheetCell.innerHTML = `<a href="${datasheet.url}" target="_blank">Datasheet Link</a>`;
+                datasheetCell.innerHTML = `<a href="${part[key].url}" target="_blank">Datasheet Link</a>`;
             } else {
                 cell.innerHTML = part[key];
             }
