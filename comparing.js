c179324ee37686230db0340138e0058f172cb651
Ruben Escarzaga <154392556+rubenzkar@users.noreply.github.com>
2024-01-02 13:39:40 -0600

    Update comparing.js

--- comparing.js
+++ comparing.js
@@ -229,12 +229,7 @@ async function displayComparisonTable() {
   var altLeakValue = getAttribute(altSpecs, 'Leakage Current');
   var altHeightValue = getAttribute(altSpecs, 'Height');
   var altLengthValue = getAttribute(altSpecs, 'Length');
-  //Temporal fix
-  //var altPrice = altPart.medianPrice1000.price;
-  var altPrice = '$Click to confirm';
-  if (altPart.medianPrice1000.price) {
-    altPrice = altPart.medianPrice1000.price
-  }
+  var altPrice = altPart?.medianPrice1000?.price;
   // Create rows for each part attribute
   const manufacturerRow = createTableRow('Manufacturer', refManufacturer, altManufacturer);
   const mpnRow = createTableRow('MPN', refMpn, altMpn);
